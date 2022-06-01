bfca95e99e69ad3cd887cee6fd671b14f3f6dd7f
developerLatConsulting <developer@latconsulting.tech>
2022-06-01 14:47:11 -0500

    initial commit

---
+++ grains/grains.js
@@ -0,0 +1,19 @@
+/**
+ * You can use the bigint type and BigInt global object to support numbers below
+ * Number.MIN_SAFE_INTEGER and above NUMBER.MAX_SAFE_INTEGER.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+ */
+
+//
+// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+    //const NUM_BASE
+export const square = (position) => {
+  throw new Error('Remove this statement and implement this function');
+};
+
+export const total = () => {
+  throw new Error('Remove this statement and implement this function');
+};
